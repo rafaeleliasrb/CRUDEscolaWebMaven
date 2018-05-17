31dffde20ba6756c8caa9b2e184935d671c650f3
Rafael Elias <13rafael.elias@gmail.com>
2018-05-16 23:31:16 -0300

    Exercicio git

---
+++ src/main/java/br/com/escola/model/domain/Calculadora.java
@@ -0,0 +1,8 @@
+package br.com.escola.model.domain;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		System.out.println("Teste operações matemáticas:");
+	}
+}
---
+++ src/main/java/br/com/escola/model/domain/OperacaoMatematica.java
@@ -0,0 +1,6 @@
+package br.com.escola.model.domain;
+
+public interface OperacaoMatematica {
+
+	public double calcula(double numeroEsquerda, double numeroDireita);
+}
